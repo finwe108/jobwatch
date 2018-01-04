7d7f7b478263d29f8f16c4f0b2619dc69d8abc96
finwe108 <nice.atma@gmail.com>
2018-01-05 01:35:32 +0800

    re-arrangement of directories

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
     <div class="container">
       <a class="navbar-brand" href="#">
         <!--<i class="fa d-inline fa-lg fa-eye"></i>-->
-        <img src="image/logo4dark32.svg"><b>  Jobwatch</b></a>
+        <img src="image/logo4dark32.svg"><b> <?php echo "Jobwatch"; ?> </b></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span> </button>
       <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
